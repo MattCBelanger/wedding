972e48ebd08d1d631e478539fbf5c4699560e939
Matthew Belanger <mbelanger@konradgroup.com>
2016-10-25 21:22:42 -0400

    init working

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,45 @@
 import { Component } from '@angular/core';
+import { PageScrollConfig } from 'ng2-page-scroll/ng2-page-scroll';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: `
+    <div class='top-container'>
+        <div class='container-fluid'>
+            <div class='row'>
+                <div class='col-xs-10'>
+                   <a pageScroll href="#info"><button>Wedding</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id='info' class='info-container'>
+        Date: 2o3842yo3r2g3
+        Reception: 23f23f2f32f
+        Time: 2342304
+    </div>
+    <div id='venue' class='venue-container'>
+        DMontebbloe
+
+    </div>
+    <div class='three-container'>
+        DMontebbloe
+
+    </div>
+    <div  class='fourth-container'>
+        DMontebbloe
+
+    </div>
+`
 })
-export class AppComponent { }
+export class AppComponent { 
+    constructor() { 
+        PageScrollConfig.defaultDuration = 500;
+      }
+
+//   public scrollElement() {
+//     let element = document.getElementById('venue');
+//     window.scrollTo(0, element.offsetTop, 500);
+//   }
+
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {Ng2PageScrollModule} from 'ng2-page-scroll/ng2-page-scroll';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, Ng2PageScrollModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
