b77e239bca5128becb010c525cf6d9a44d127e1f
Matthew Belanger <mbelanger@konradgroup.com>
2016-10-30 13:05:18 -0400

    v1

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,45 +6,61 @@ import { PageScrollConfig } from 'ng2-page-scroll/ng2-page-scroll';
     template: `
     <div class='wed-container'>
         <div class='top-navbar'>
-            <div class='title'>
-                July 8th 2017
-            </div>
             <div class='nav-options'>
                 <a pageScroll href="#where">Where</a>
                 <a pageScroll href="#when">When</a>
                 <a pageScroll href="#info">Info</a>
-                 <a pageScroll href="#misc">Misc</a>
+                <a pageScroll href="#misc">Misc</a>
             </div>
         </div>
         <div class='top-container'>
             <img [class]="classMap" (click)="clicked()" [src]="urlVariable" />
-            <i *ngIf="showPlay" (click)="clicked()" class="fa fa-play-circle fa-3x" aria-hidden="true"></i>
+            <div class='title'> 
+            Come join us and party!
+            </div>
         </div>
         <div id='where' class='info-container'>
-            <div class="title">
-                Where?
+            <div class='container-fluid'>
+                <div class='row'>
+                    <div class='col-xs-6'>
+                        <div class="title">
+                            Where?
+                        </div>
+                        <div class='content'>
+                            Fairmont Le Château Montebello, Rue Notre Dame, Montebello, QC
+                            <a href='https://www.google.ca/maps/place/Fairmont+Le+Ch%C3%A2teau+Montebello/@45.6453106,-74.9517356,17z/data=!3m1!4b1!4m5!3m4!1s0x4cce584bdcb2a6a7:0x2b2530aaf408ea45!8m2!3d45.6453106!4d-74.9495469' target='_blank' >Montebello on Google Maps</a>
+                        </div>
+                    </div>
+                    <div class='col-xs-6'>
+                        <img class='img-responsive' src='http://www.fodors.com/images/pois-thumbs/Exterior-483844-0-full.jpg' />
+                    </div>
+                </div>
             </div>
-           <div class='content'>
-             Fairmont Le Château Montebello, Rue Notre Dame, Montebello, QC
-           </div>
-            <a href='https://www.google.ca/maps/place/Fairmont+Le+Ch%C3%A2teau+Montebello/@45.6453106,-74.9517356,17z/data=!3m1!4b1!4m5!3m4!1s0x4cce584bdcb2a6a7:0x2b2530aaf408ea45!8m2!3d45.6453106!4d-74.9495469' target='_blank' >Montebello on Google Maps</a>
         </div>
         <div id='when' class='venue-container'>
-            <div class="title">
-                When?
+            <div class='container-fluid'>
+                <div class='row'>
+                    <div class='col-xs-3'>
+                        <div class="title">
+                            When?
+                        </div>
+                    </div>
+                    <div class='col-xs-7 col-xs-offset-2'>
+                        <div class='content'>
+                            July 8th 2017
+                        </div>
+                            <div class='content'>
+                            Ceremony 5:30pm
+                        </div>
+                        <div class='content'>
+                            Cocktails 6:30pm - 7:30pm
+                        </div>
+                        <div class='content'>
+                            Dinner & Recption starts at 7:30pm
+                        </div>
+                    </div>
+                </div>
             </div>
-           <div class='content'>
-            July 8th 2017
-           </div>
-            <div class='content'>
-            Ceremony 5:30pm
-           </div>
-           <div class='content'>
-            Cocktails 6:30pm - 7:30pm
-           </div>
-           <div class='content'>
-            Dinner & Recption starts at 7:30pm
-           </div>
         </div>
         <div id='info' class='three-container'>
             <div class="title">
@@ -78,15 +94,16 @@ export class AppComponent {
     videoTimeOut: any;
     classMap: any;
     constructor() { 
-        PageScrollConfig.defaultDuration = 500;
+        PageScrollConfig.defaultDuration = 800;
         this.urlVariable = 'images/EdgeWalk-41.jpg';
         this.showPlay = true;
         this.classMap = {'viewHeight': false};
     }
 
     clicked() {
+        //  <i *ngIf="showPlay" class="fa fa-play-circle fa-3x" aria-hidden="true"></i>
         if (this.urlVariable === 'images/EdgeWalk-41.jpg') {
-            this.urlVariable = 'images/croped-256.gif';
+            this.urlVariable = 'http://i.imgur.com/NADwJbx.gifv';
             this.showPlay = false;
             this.classMap = {'viewHeight': true};
             this.videoTimeOut = setTimeout(() => {
