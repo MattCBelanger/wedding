417d9f74a145e5019bd537c9c9e47677c1b5d22f
Matthew Belanger <mbelanger@konradgroup.com>
2016-11-05 13:43:55 -0400

    1.0.0

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,14 +9,19 @@ import { PageScrollConfig } from 'ng2-page-scroll/ng2-page-scroll';
             <div class='nav-options'>
                 <a pageScroll href="#where">Where</a>
                 <a pageScroll href="#when">When</a>
-                <a pageScroll href="#info">Info</a>
+                <a pageScroll href="#info">Booking</a>
                 <a pageScroll href="#misc">Misc</a>
             </div>
         </div>
         <div class='top-container'>
-            <img [class]="classMap" [src]="urlVariable" />
-            <div class='title'> 
-            Come join us and party!
+            <div class='title1'>
+                Tiia and Matt Wedding 
+            </div>
+            <div class='title2'>
+                July 8th 2017
+            </div>
+            <div class='title3'> 
+                Come join us and party!
             </div>
         </div>
         <div id='where' class='info-container'>
@@ -24,11 +29,24 @@ import { PageScrollConfig } from 'ng2-page-scroll/ng2-page-scroll';
                 <div class='row'>
                     <div class='col-xs-6'>
                         <div class="title">
-                            Where?
+                            Where
                         </div>
                         <div class='content'>
-                            Fairmont Le Château Montebello, Rue Notre Dame, Montebello, QC
+                            Fairmont Le Château Montebello 
+                            <div class='address'>
+                                <div>392 Notre Dame</div>
+                                <div> Quebec, Canada </div>
+                                <div>  J0V 1L0 </div>
+                            </div>
+                        </div>
+                        <div class='content'>
                             <a href='https://www.google.ca/maps/place/Fairmont+Le+Ch%C3%A2teau+Montebello/@45.6453106,-74.9517356,17z/data=!3m1!4b1!4m5!3m4!1s0x4cce584bdcb2a6a7:0x2b2530aaf408ea45!8m2!3d45.6453106!4d-74.9495469' target='_blank' >Montebello on Google Maps</a>
+                        </div>
+                        <div class='content'>
+                            <a href='http://www.fairmont.com/montebello/map/mapanddirections/' target='_blank' >Directions to Montebello</a>
+                        </div>
+                        <div class='content'>
+                            <a href='http://www.fairmont.com/montebello/' target='_blank' >Montebello's Website</a>
                         </div>
                     </div>
                     <div class='col-xs-6'>
@@ -40,23 +58,38 @@ import { PageScrollConfig } from 'ng2-page-scroll/ng2-page-scroll';
         <div id='when' class='venue-container'>
             <div class='container-fluid'>
                 <div class='row'>
-                    <div class='col-xs-3'>
-                        <div class="title">
-                            When?
-                        </div>
+                    <div class='col-xs-6'>
+                        <img class='img-responsive' src='images/gazeebo.jpg' />
                     </div>
-                    <div class='col-xs-7 col-xs-offset-2'>
-                        <div class='content'>
+                    <div class='col-xs-6'>
+                        <div class="title">
+                            When
+                        </div>
+                        <div class='content bot-pad'>
                             July 8th 2017
                         </div>
-                            <div class='content'>
-                            Ceremony 5:30pm
+                            <div class='content2'>
+                            Ceremony 5:30pm - Water Gazebo
                         </div>
-                        <div class='content'>
-                            Cocktails 6:30pm - 7:30pm
+                        <div class='content2'>
+                            Cocktails 6:30pm - Golf ClubHouse
                         </div>
-                        <div class='content'>
-                            Dinner & Recption starts at 7:30pm
+                        <div class='content2'>
+                            Dinner & Reception 7:30pm - Golf ClubHouse
+                        </div>
+                    </div>
+                </div>
+                <div class='row'>
+                    <div class='col-xs-12 center pad-top'>
+                        <div class='content smaller-font'>
+                            Shuttles will be provided to and from the main lobby and the clubhouse, before cocktail hour and during the reception.
+                        </div>
+                    </div>
+                </div>
+                <div class='row'>
+                    <div class='col-xs-12 center pad-top'>
+                        <div class='content smaller-font'>
+                             The ceremony will be outdoors on the lawn, weather permitting. Otherwise it will be inside in the Quebec Room.
                         </div>
                     </div>
                 </div>
@@ -67,10 +100,19 @@ import { PageScrollConfig } from 'ng2-page-scroll/ng2-page-scroll';
                 Booking Info
             </div>
             <div class='content'>
-                We have a block of rooms resvered at Montebello for the wedding. We will be looking up alternatives in the area if you wish you not stay at the hotel. ( But it IS amazing! :D)
+                We have a block of rooms reserved at Montebello for July 8th. To make reservations and take advantage of our group rate follow the link below.
+           </div>
+            <div class='content'>
+               <a class='purple' href='https://aws.passkey.com/go/merikalliobelanger' target='_blank' >Booking Website</a>
            </div>
            <div class='content'>
-                Information for booking and discount code to come.
+               If you do not wish to stay at Montebello there are plenty of B&Bs in there area. Here are some that we found.
+           </div>
+           <div class='content'>
+                <a class='purple' href='http://www.auxdouceursdupassant.com/eng/acceuil.html' target='_blank' >Aux Douceurs du Passant</a>
+           </div>
+           <div class='content'>
+                <a class='purple' href='http://www.aubergemontebello.com/' target='_blank'>Auberge Montebello</a>
            </div>
         </div>
         <div id='misc' class='misc-container'>
@@ -78,11 +120,11 @@ import { PageScrollConfig } from 'ng2-page-scroll/ng2-page-scroll';
             Misc
           </div>
           <div class='content'>
-            <a href='http://www.fairmont.com/montebello/' target=''_blank>Montebello Website</a>
+            Your presence at our wedding is the best gift we could ask for. 
+            If you wish to contribute to our honeymoon, it would be very appriciated!
           </div>
           <div class='content'>
-            Your presence at our wedding in the best present we could ask for. 
-            If you did wish to contribute to our honeymoon it would be very appriciated!
+            We are looking forward to seeing all of you!
           </div>
         </div>
     </div>
